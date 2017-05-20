61eb6c919699fd887b2d24b3e8744fe726009709
mebegu <mehmetgurcay@ku.edu.tr>
2017-05-20 16:36:15 +0300

    init

---
+++ functions/index.js
@@ -0,0 +1,46 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+/**
+ * Triggers when a user gets a new follower and sends a notification.
+ *
+ * Followers add a flag to `/followers/{followedUid}/{followerUid}`.
+ * Users save their device notification tokens to `/users/{followedUid}/notificationTokens/{notificationToken}`.
+ */
+exports.sendNotification = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  console.log("Body: "+ req.body);
+  const receiver = req.body.receiver;
+  const sub = req.body.sub;
+  const text = req.body.text;
+
+  // Get the list of device notification tokens.
+  const getDeviceTokensPromise = admin.database().ref(`devices`).child(receiver).once('value').then(result => {
+    const tokensSnapshot = result;
+
+    // Check if there are any device tokens.
+    if (!tokensSnapshot) {
+      return console.log('There are no notification tokens to send to.');
+    }
+    console.log('There is', tokensSnapshot, 'tokens to send notifications to.');
+
+    // Notification details.
+    const payload = {
+      notification: {
+        title: sub,
+        body: text
+      }
+    };
+
+    // Listing all tokens.
+    const token = tokensSnapshot.val();
+    console.log("token : "+token);
+
+    // Send notifications to all tokens.
+    return admin.messaging().sendToDevice(token, payload).then(response => {
+      return res.send("ok");
+    });
+  });
+});
+
